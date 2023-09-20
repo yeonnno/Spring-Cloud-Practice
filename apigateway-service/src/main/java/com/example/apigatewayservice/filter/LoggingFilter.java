a4e14b7e2de4aca339612656c8d7665b59729a00
yyh0126 <yyh0126@naver.com>
2023-09-20 22:15:07 +0900

    Feat : Logging Filter

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -3,7 +3,9 @@ package com.example.apigatewayservice.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -11,32 +13,33 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
 
-    public GlobalFilter() {
+    public LoggingFilter() {
         super(Config.class);
     }
 
     @Override
     public GatewayFilter apply(Config config) {
-        // Custom Pre Filter
-        return ((exchange, chain) -> {
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Global Filter baseMessage: {}", config.getBaseMessage());
+            log.info("Logging Filter baseMessage: {}", config.getBaseMessage());
 
             if (config.isPreLogger()) {
-                log.info("Global Filter Start: request id -> {}", request.getId());
+                log.info("Logging PRE Filter: request id -> {}", request.getId());
             }
 
             // Custom Post Filter
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 if (config.isPostLogger()) {
-                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
+                    log.info("Logging POST Filter: response code -> {}", response.getStatusCode());
                 }
             }));
-        });
+        }, Ordered.HIGHEST_PRECEDENCE);
+
+        return filter;
     }
 
     @Data
