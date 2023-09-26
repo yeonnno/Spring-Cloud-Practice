611173475addfc593b61bfd304d38cf9ef3c9c67
yyh0126 <yyh0126@naver.com>
2023-09-26 18:02:18 +0900

    Feat : ErrorDecoder를 이용한 예외처리

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -60,12 +60,15 @@ public class UserServiceImpl implements UserService {
 
         /* Using a Feign Client */
         /* Feign Exception handling */
-        List<ResponseOrder> orderList = null;
-        try {
-            orderList = orderServiceClient.getOrders(userId);
-        } catch (FeignException e) {
-            log.error(e.getMessage());
-        }
+//        List<ResponseOrder> orderList = null;
+//        try {
+//            orderList = orderServiceClient.getOrders(userId);
+//        } catch (FeignException e) {
+//            log.error(e.getMessage());
+//        }
+
+        /* ErrorDecoder */
+        List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
 
         userDto.setOrders(orderList);
 
